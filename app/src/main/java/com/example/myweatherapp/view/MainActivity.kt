2d6768a9dc10861b868d0c9c853af1eb4c1c9187
Lliril <kirya.ermalenok@mail.ru>
2021-11-28 22:19:22 +0300

    commit1

---
+++ app/src/main/java/com/example/myweatherapp/view/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myweatherapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.myweatherapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setBottomMenu()
+    }
+    private fun setBottomMenu() {
+        val navController = findNavController(R.id.myNavHostFragment)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_menu)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
